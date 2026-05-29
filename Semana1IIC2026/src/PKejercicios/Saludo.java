bc801b605c39fddad7b35d278f83f3032a191122
stevenarias65 <44099803+stevenarias65@users.noreply.github.com>
2026-05-28 20:28:13 -0600

    Semana 1
    Ejercicio semana 1

---
+++ Semana1IIC2026/src/PKejercicios/Saludo.java
@@ -0,0 +1,29 @@
+
+package PKejercicios;
+
+
+public class Saludo {//inicia la clase
+    
+    //metodo main
+    public static void main(String[] args){//inicio del metodo
+       
+        System.out.println("Hola mundo");
+        
+        boolean estado;//Creando una variable booleana
+        estado = true; 
+        
+        System.out.println(estado);
+        
+        int numero = 20;
+        int numero2 = 20;
+        
+        int resultado = numero + numero2;
+        System.out.println(resultado);
+     
+        
+    }//fin del metodo
+    
+
+
+}//Aqui termina la clase
+
